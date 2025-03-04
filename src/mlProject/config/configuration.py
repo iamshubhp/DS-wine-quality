0e5798beb20c821c625fe187b0af87eea99b31d1
iamshubhp <shubhpatel1812002@gmail.com>
2025-03-03 20:07:53 -0500

    data validation done

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,7 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import (DataIngestionConfig)
+from mlProject.entity.config_entity import (
+    DataIngestionConfig, DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -30,20 +31,20 @@ class ConfigurationManager:
 
         return data_ingestion_config
 
-    # def get_data_validation_config(self) -> DataValidationConfig:
-    #     config = self.config.data_validation
-    #     schema = self.schema.COLUMNS
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
 
-    #     create_directories([config.root_dir])
+        create_directories([config.root_dir])
 
-    #     data_validation_config = DataValidationConfig(
-    #         root_dir=config.root_dir,
-    #         STATUS_FILE=config.STATUS_FILE,
-    #         unzip_data_dir=config.unzip_data_dir,
-    #         all_schema=schema,
-    #     )
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema,
+        )
 
-    #     return data_validation_config
+        return data_validation_config
 
     # def get_data_transformation_config(self) -> DataTransformationConfig:
     #     config = self.config.data_transformation
